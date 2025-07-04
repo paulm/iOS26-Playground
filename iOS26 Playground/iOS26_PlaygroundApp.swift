7fa7262c5fa07ebaa64a2b82cbdc57cda7a179c5
Paul Mayne <pmayne@gmail.com>
2025-07-04 14:08:25 -0600

    Initial Commit

---
+++ iOS26 Playground/iOS26_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS26_PlaygroundApp.swift
+//  iOS26 Playground
+//
+//  Created by Paul Mayne on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS26_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
